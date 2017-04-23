056f87c6d6089f2fe48e03e1f953ab02ffa228c1
abrikosas <algirdas.klimavicius@gmail.com>
2017-04-23 14:03:23 +0200

    removed App

--- src/main/scala/GeneralTopic.scala
+++ src/main/scala/GeneralTopic.scala
@@ -6,31 +6,32 @@ import org.apache.spark.streaming.kafka._
 import org.apache.spark.SparkConf
 
 
-object DirectKafkaWordCount extends App {
+object DirectKafkaWordCount  {
 
   // StreamingExamples.setStreamingLogLevels()
 
+  def main(args: Array[String]): Unit = {
 
+    // Create context with 2 second batch interval
+    val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
 
-  // Create context with 2 second batch interval
-  val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
-  val ssc = new StreamingContext(sparkConf, Seconds(2))
+    // Create direct kafka stream with brokers and topics
 
-  // Create direct kafka stream with brokers and topics
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> "fmak.lt:9092",
+      "zookeeper.connect" -> "fmak.lt:2181")
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+      ssc, kafkaParams, Set("general"))
 
-  val kafkaParams = Map[String, String]("metadata.broker.list" -> "fmak.lt:9092",
-    "zookeeper.connect" -> "fmak.lt:2181")
-  val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-    ssc, kafkaParams, Set("general"))
+    // Get the lines, split them into words, count the words and print
+    val lines = messages.map(_._2)
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
+    wordCounts.print()
 
-  // Get the lines, split them into words, count the words and print
-  val lines = messages.map(_._2)
-  val words = lines.flatMap(_.split(" "))
-  val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-  wordCounts.print()
+    // Start the computation
+    ssc.start()
+    ssc.awaitTermination()
 
-  // Start the computation
-  ssc.start()
-  ssc.awaitTermination()
-
+  }
 }
